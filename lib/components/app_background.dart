a784048d160c10e64e9c50e79152c8b114351122
Mozart Rueckert <mozartrue@outlook.com>
2023-09-13 23:38:10 -0300

    feat: create background component

---
+++ lib/components/app_background.dart
@@ -0,0 +1,32 @@
+import 'package:buylist/constants.dart';
+import 'package:flutter/material.dart';
+
+class AppBackground extends StatelessWidget {
+  final Widget child;
+  const AppBackground({
+    required this.child,
+    super.key
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    return Stack(
+      children: [
+        Container( 
+          height: size.height,
+          width: size.width,
+          color: bkgColor,
+        ),
+        SizedBox(
+          width: size.width,
+          child: Image.asset(
+            "assets/images/cover_image.jpg",
+            fit: BoxFit.cover,
+          ),
+        ), 
+        child,
+      ]
+    );
+  }
+}
