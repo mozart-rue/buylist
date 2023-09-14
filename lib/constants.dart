5046dd931acf418389b656010dedc76082be6165
Mozart Rueckert <mozartrue@outlook.com>
2023-09-14 19:23:15 -0300

    feat: add colors to project

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,32 @@
 import 'package:flutter/material.dart';
 
-const bkgColor = Color(0xff0C0C0D);
+
+// Cores Base
+const gray_100 = Color(0xffFBF9FE);
+const gray_200 = Color(0xffAFABB6);
+const gray_300 = Color(0xff252529);
+const gray_400 = Color(0xff17171A);
+const gray_500 = Color(0xff111112);
+const gray_600 = Color(0xff0C0C0D);
+
+// Cores Produto
+const purpleLight = Color(0xffA881E6);
+const purple = Color(0xff7450AC);
+const purpleDark = Color(0xff523480);
+
+// Cores Feedback
+const successLight = Color(0xff4E995E);
+const success = Color(0xff2F723D);
+
+// Cores de Apoio
+const pink = Color(0xffDB5BBF);
+const pinkDark = Color(0xff251622);
+const orange = Color(0xffE07B67);
+const orangeDark = Color(0xff261A17);
+const yellow = Color(0xffBB9F3A);
+const yellowDark = Color(0xff211E12);
+const green = Color(0xff8CAD51);
+const greenDark = Color(0xff1C2015);
+const blue = Color(0xff7B94CB);
+const blueDark = Color(0xff1A1D23);
 
