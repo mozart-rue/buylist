e1a3f9a3d419eec185c9e2c25d34de7af65dbd49
Mozart Rueckert <mozartrue@outlook.com>
2023-09-13 23:01:26 -0300

    feat: added HomeScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buylist/screens/home_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(),
+      home: const HomeScreen(),
     );
   }
 }
