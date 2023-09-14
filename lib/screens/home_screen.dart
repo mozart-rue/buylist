0bde4db70bab2201d702107382b66289f2d3e4d4
Mozart Rueckert <mozartrue@outlook.com>
2023-09-14 20:25:24 -0300

    feat: set title

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:buylist/components/app_background.dart';
+import 'package:buylist/constants.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -6,9 +7,24 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: AppBackground(
-        child: Container(),
+        child: Center(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top: 20)),
+              Text(
+                'Lista De Compras',
+                textAlign: TextAlign.start,
+                style: TextStyle( 
+                  color: gray_100,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
