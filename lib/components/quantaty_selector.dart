6fa3fa2471a915eb520519273c78683298a28f87
Mozart Rueckert <mozartrue@outlook.com>
2023-09-18 19:34:40 -0300

    feat: create component set quantatiy volume

---
+++ lib/components/quantaty_selector.dart
@@ -0,0 +1,86 @@
+import 'package:buylist/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+class QuantatiSelector extends StatefulWidget {
+  const QuantatiSelector({super.key});
+
+  @override
+  State<QuantatiSelector> createState() => _QuantatiSelectorState();
+}
+
+class _QuantatiSelectorState extends State<QuantatiSelector> {
+  List<String> mesureMetricsValues = ['UN.', 'KG'];
+  String? dropMetricValue;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Text(
+          'Quantidade',
+          style: TextStyle(
+            color: gray_200,
+            fontSize: 12,
+          ),
+        ),
+        Row(
+          children: [
+            Container(
+              height: 40,
+              width: 66,
+              decoration: const BoxDecoration(
+                color: gray_500,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(6), 
+                  bottomLeft: Radius.circular(6)
+                ),
+              ),
+              child: const Center(
+                child: TextField(
+                  textAlign: TextAlign.center,
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: "0",
+                    hintStyle: TextStyle(
+                      color: gray_100,
+                    ),
+                  ),
+                  style: TextStyle(
+                    color: gray_100,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              height: 40,
+              width: 66,
+              decoration: const BoxDecoration(
+                color: gray_400,
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(6), 
+                  bottomRight: Radius.circular(6)
+                ),
+              ),
+              child: DropdownButton(
+                value: dropMetricValue ?? mesureMetricsValues.first,
+                onChanged: (value) {
+                  setState(() {
+                    dropMetricValue = value!;               
+                  }); 
+                },
+                items: mesureMetricsValues.map<DropdownMenuItem<String>>((String value) {
+                  return DropdownMenuItem<String>(
+                    value: value,
+                    child: Text(value),
+                  );
+                }).toList(),
+              ),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
